23c51f1d83273282896d967b71a1fc85342b547b
nilammone <nilammone.bannavong@gmail.com>
2021-12-29 08:52:30 +0700

    sucess admin api

--- app/Models/LinkProduct.php
+++ app/Models/LinkProduct.php
@@ -12,6 +12,12 @@ use Illuminate\Database\Eloquent\Model;
  * @method static \Illuminate\Database\Eloquent\Builder|LinkProduct newQuery()
  * @method static \Illuminate\Database\Eloquent\Builder|LinkProduct query()
  * @mixin \Eloquent
+ * @property int $id
+ * @property int $link_id
+ * @property int $product_id
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkProduct whereId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkProduct whereLinkId($value)
+ * @method static \Illuminate\Database\Eloquent\Builder|LinkProduct whereProductId($value)
  */
 class LinkProduct extends Model
 {
